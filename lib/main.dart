3832b0d2f311598b3a336aa470579e964c9b14b1
angelgoitia <angelgoitia1995@gmail.com>
2021-08-02 19:26:58 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:ctbeca/controller/mainController.dart';
 import 'package:ctbeca/env.dart';
 
@@ -9,24 +10,54 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
+
+    var size = MediaQuery.of(context).size;
+
     return GetMaterialApp(
-      title: 'Reto Flutter',
+      title: 'CTBeca',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: GetBuilder<MainController>(
-      init: MainController(),
-      builder: (_home) => Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(
-            color: colorPrimary,
-          )
-        )
+        init: MainController(),
+        builder: (_home) => WillPopScope(
+          onWillPop: () async =>false,
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image(
+                    image: AssetImage("assets/axies.png"),
+                    width: size.width/2,
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top:5, bottom: 5),
+                    child: AutoSizeText(
+                      "Cargando...",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'MontserratSemiBold',
+                      ),
+                      maxFontSize: 24,
+                      minFontSize: 24,
+                    ),
+                  ),
+                  CircularProgressIndicator(
+                    valueColor: new AlwaysStoppedAnimation<Color>(colorPrimary),
+                  ),
+                ]
+              ),
+            ),
+          ),
+        ),
       ),
-    ),
     );
   }
 
